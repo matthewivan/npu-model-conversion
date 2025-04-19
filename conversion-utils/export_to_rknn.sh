a0025f0ee2301f838b1533426119fb8cb748c7f9
matthewivan <widjajamatthewivan@gmail.com>
2025-04-19 02:23:24 +0000

    Add all the key functions

---
+++ conversion-utils/export_to_rknn.sh
@@ -0,0 +1,2 @@
+python3 convert.py ../yolov8n.onnx rk3566 i8 ../yolov8n.rknn # for boards with rk3566 chip
+# python3 convert.py /path/to/model.onnx rk3588 i8 /path/to/new/model.rknn # for boards with rk3588 chip
---
+++ conversion-utils/rknn-toolkit2-setup.sh
@@ -0,0 +1,2 @@
+pip install -r ./rknn-toolkit2-build/x86_64/requirements_cp311-2.3.2.txt
+pip install ./rknn-toolkit2-build/x86_64/rknn_toolkit2-2.3.2-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
